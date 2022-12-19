df355d28af132bd5c09bc83bd00a53d8be33d88c
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 04:20:31 +0530

    program 2 done

---
+++ 2/Exp2/Exp2/Prgrm2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp2
+{
+    class Prgrm2
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 0; i<args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
