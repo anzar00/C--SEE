32cd547b6e6db95cbedd53367e68a2aad6b70146
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 05:11:52 +0530

    program 4 done

---
+++ 4/Exp4/Exp4/Prgrm4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp4
+{
+    class Prgrm4
+    {
+        public static void Main(string[] args)
+        {
+            int n = 2;
+            for(int i =1; i<=10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                else if (i == 8)
+                {
+                    for(int j = 0; j < 3; j++)
+                    {
+                        Console.WriteLine("2 * " + i + " = " + n * i);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("2 * " + i + " = " + n * i);
+                }
+            }
+        }
+    }
+}
