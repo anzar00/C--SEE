aa973328a5e5d7af0a1b9f0c69c7d2bc49742827
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 03:50:57 +0530

    program 1 done

---
+++ 1/Exp1/Exp1/Prgrm1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp1
+{
+    public class Prgrm1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+            Prgrm1a.Main(new string[] { });
+
+            float var1 = (float)100.99;
+            double var2 = 100.0;
+
+            Console.WriteLine("Var1 value is - " + var1);
+            Console.WriteLine("Var2 value is - " + var2);
+        }
+    }
+}
---
+++ 1/Exp1/Exp1/Prgrm1a.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp1
+{
+    public class Prgrm1a
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
