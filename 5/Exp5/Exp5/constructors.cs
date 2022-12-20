118606eb447d72a785b418b909830927d1ab92cd
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 07:22:18 +0530

    5

---
+++ 5/Exp5/Exp5/constructors.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp5
+{
+    class constructors
+    {
+        public static void Main(string[] args)
+        {
+            student1 s1 = new student1();
+
+            Console.WriteLine(s1.getstudentdetails1().rollnumber);
+
+            Console.WriteLine(s1.getstudentdetails());
+
+            Console.ReadKey();
+        }
+    }
+    class student1
+    {
+        public int rollnumber;
+        public string name;
+        public bool isactive;
+        public student1()
+        {
+            rollnumber = 101;
+            name = "priya";
+            isactive = true;
+        }
+        public student1 returnstudentdetails()
+        {
+            return this;
+        }
+        public string getstudentdetails()
+        {
+            return name;
+        }
+        public student1 getstudentdetails1()
+        {
+            return this;
+        }
+
+    }
+
+}
---
+++ 5/Exp5/Exp5/ordinary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp5
+{
+    class ordinary
+    {
+        public static void Main(string[] args)
+        {
+            student s = new student();
+
+            Console.WriteLine(s.name);
+            s.enterstudentdetails();
+            Console.WriteLine(s.name);
+            Console.ReadKey();
+        }
+    }
+
+    class student
+    {
+        public int rollnumber;
+        public string name;
+        public bool isactive;
+        public student returnstudentdetails()
+        {
+            return this;
+        }
+
+        public student enterstudentdetails(string s)
+        {
+            name = s;
+        }
+    }
+}
