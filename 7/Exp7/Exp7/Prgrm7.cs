efbf90af5338b3bc4dd16973313a34061f686228
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 06:59:37 +0530

    prog 7

---
+++ 7/Exp7/Exp7/Prgrm7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp7
+{
+    class Prgrm7
+    {
+        public static void Main(string[] args)
+        {
+            class7 obj = new class7();
+            obj.rollnumber = -10;
+            obj.printvalue();
+            obj.setvalue(5);
+            obj.printvalue();
+        }
+
+        class class7
+        {
+            private int _rollnumber; // cannot use outside class
+            public int rollnumber // bcoz of property can use roll number outside
+            {
+                get { return _rollnumber; }
+                set { _rollnumber = value; }
+            }
+            public void printvalue()
+            {
+                int i = rollnumber;
+                Console.WriteLine(i);
+            }
+            public void setvalue(int rollnumber)
+            {
+                this.rollnumber = rollnumber;
+            }
+        }
+    }
+}
