cf70251994faadf2c1c42e8b8f024d0f5529a9ab
anzar00 <peerzada.anzar@gmail.com>
2022-12-20 07:05:43 +0530

    Program 9

---
+++ 8/Exp8/Exp8/Prgrm8.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp8
+{
+    class Prgrm8
+    {
+        public void calculatePerimeter(int length, int sides)
+        {
+            int result = length * sides;
+            Console.WriteLine("Perimeter: " + result);
+        }
+    }
+    class Square : Prgrm8
+    {
+        public int length = 200;
+        public int sides = 4;
+        public void calculateArea()
+        {
+            int area = length * length;
+            Console.WriteLine("Area of Square: " + area);
+        }
+    }
+    class Rectangle : Prgrm8
+    {
+        public int length = 100;
+        public int breadth = 200;
+        public int sides = 4;
+        public void calculateArea()
+        {
+            int area = length * breadth;
+            Console.WriteLine("Area of Rectangle: " + area);
+        }
+    }
+
+    class class8
+    {
+        static void Main(string[] args)
+        {
+            Square s1 = new Square();
+            s1.calculateArea();
+            s1.calculatePerimeter(s1.length, s1.sides);
+            Rectangle t1 = new Rectangle();
+            t1.calculateArea();
+            t1.calculatePerimeter(t1.length, t1.sides);
+        }
+    }
+}
---
+++ 9/Exp9/Exp9/Prgrm9.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp9
+{
+    class Prgrm9
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            string[] arr1 = new string[5];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine("please enter array elements " + arr1[i]);
+                arr1[i] = Console.ReadLine();
+            }
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+            int[] arr2 = new int[5];
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine("please enter array elements ");
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (var item in arr2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
